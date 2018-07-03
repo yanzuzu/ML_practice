4f01b0d9b4a358e4be59fbdc0992739e3965bef5
Sam Lo <sam@randomidea.com>
2018-07-03 17:07:02 +0800

    Maze walker practice.

---
+++ ML_Practice/Assets/MazeWalkers/Scripts/MazeBrain.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeBrain : MonoBehaviour 
+{
+	private const int DNA_LENGTH = 2;
+	/*
+	 0: forward
+	 1: back
+	 2: left
+	 3: right
+	 */
+	private const int DNA_MAXVALUE = 4;
+
+	public DNA Dna;
+	public float Distance;
+	private Vector3 m_startPos;
+
+	public void Init()
+	{
+		Dna = new DNA (2, 4);
+		m_startPos = Vector3.zero;
+	}
+
+	void OnCollisionEnter(Collision obj)
+	{
+		if(obj.gameObject.tag == "dead")
+		{
+			this.gameObject.SetActive (false);
+			Distance = 0f;
+		}
+	}
+
+	private	void Update()
+	{
+		RaycastHit hit;
+		Physics.Raycast (transform.position, transform.forward * 2f, out hit);
+		bool isHitWall = false;;
+		if (hit.collider != null)
+		{
+			if (hit.collider.gameObject.tag == "wall")
+			{
+				isHitWall = true;
+			}
+		}
+
+		float move = 0f;
+		float turn = 0f;
+
+		if (isHitWall)
+		{
+			if (Dna.GetGene (1) == 0)
+			{
+				move = 1f;
+			} else if (Dna.GetGene (1) == 1)
+			{
+				move = -1f;
+			} else if (Dna.GetGene (1) == 2)
+			{
+				turn = 1f;
+			} else if (Dna.GetGene (1) == 3)
+			{
+				turn = -1f;
+			}
+
+		} else
+		{
+			if (Dna.GetGene (0) == 0)
+			{
+				move = 1f;
+			} else if (Dna.GetGene (0) == 1)
+			{
+				move = -1f;
+			} else if (Dna.GetGene (0) == 2)
+			{
+				turn = 1f;
+			} else if (Dna.GetGene (0) == 3)
+			{
+				turn = -1f;
+			}
+		}
+
+		transform.Translate (new Vector3 (0, 0, move * 5f * Time.deltaTime));
+		transform.Rotate( new Vector3(0,90f * turn , 0));
+
+		Distance = Vector3.Distance (m_startPos, transform.position);
+	}
+
+}
---
+++ ML_Practice/Assets/MazeWalkers/Scripts/MazeManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class MazeManager : MonoBehaviour 
+{
+
+	public GameObject botPrefab;
+	public int populationSize = 50;
+	List<MazeBrain> population = new List<MazeBrain>();
+	public static float elapsed = 0;
+	public float trialTime = 10;
+	int generation = 1;
+
+	public GameObject StartPosObj;
+	public GameObject EndPosObj;
+
+	GUIStyle guiStyle = new GUIStyle();
+	void OnGUI()
+	{
+		guiStyle.fontSize = 25;
+		guiStyle.normal.textColor = Color.white;
+		GUI.BeginGroup (new Rect (10, 10, 250, 150));
+		GUI.Box (new Rect (0,0,140,140), "Stats", guiStyle);
+		GUI.Label(new Rect (10,25,200,30), "Gen: " + generation, guiStyle);
+		GUI.Label(new Rect (10,50,200,30), string.Format("Time: {0:0.00}",elapsed), guiStyle);
+		GUI.Label(new Rect (10,75,200,30), "Population: " + population.Count, guiStyle);
+		GUI.EndGroup ();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < populationSize; i++)
+		{
+			GameObject b = Instantiate(botPrefab, StartPosObj.transform.position, StartPosObj.transform.rotation);
+			MazeBrain brain = b.GetComponent<MazeBrain> ();
+			brain.Init();
+			population.Add(brain);
+		}
+	}
+
+	MazeBrain Breed(MazeBrain parent1, MazeBrain parent2)
+	{
+		GameObject offspring = Instantiate(botPrefab,StartPosObj.transform.position, StartPosObj.transform.rotation);
+		MazeBrain b = offspring.GetComponent<MazeBrain>();
+		if(Random.Range(0,100) == 1) //mutate 1 in 100
+		{
+			b.Init();
+			b.Dna.Mutate();
+		}
+		else
+		{ 
+			b.Init();
+			b.Dna.Combine(parent1.Dna,parent2.Dna);
+		}
+		return b;
+	}
+
+	void BreedNewPopulation()
+	{
+		List<MazeBrain> sortedList = population.OrderBy(o => o.Distance).ToList();
+
+		population.Clear();
+		for (int i = (int) (sortedList.Count / 2.0f) - 1; i < sortedList.Count - 1; i++)
+		{
+			population.Add(Breed(sortedList[i], sortedList[i + 1]));
+			population.Add(Breed(sortedList[i + 1], sortedList[i]));
+		}
+		//destroy all parents and previous population
+		for(int i = 0; i < sortedList.Count; i++)
+		{
+			Destroy(sortedList[i].gameObject);
+		}
+		generation++;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		elapsed += Time.deltaTime;
+		if(elapsed >= trialTime)
+		{
+			BreedNewPopulation();
+			elapsed = 0;
+		}
+	}
+}
